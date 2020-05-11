09ac379ee5ab538ae900f588f7bbcc0ff9d5b10e
ogawa4study <ogawa4study@gmail.com>
2020-05-11 02:28:51 +0000

    CRUD tasks

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,5 +3,21 @@
 @section('content')
 
 <!--処理-->
+    <h1>新規タスクの作成ページ</h1>
+    
+    <div class="row">
+        <div class="col-6">
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク：') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+                
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -3,5 +3,22 @@
 @section('content')
 
 <!--処理-->
+    <h1>id: {{ $task->id }} のタスク編集ページ</h1>
+    
+    <div class="row">
+        <div class="cpl-6">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('更新', ['class' => 'btn btn-primary']) !!}
+                
+            {!! Form::close() !!}
+        </div>
+    </div>
+
 
 @endsection
