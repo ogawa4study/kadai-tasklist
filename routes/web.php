19782b5bdd198f7969b67118a2703b004dab7e56
ogawa4study <ogawa4study@gmail.com>
2020-05-15 06:22:50 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,19 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function() {
+    return view('welcome');
+});
 
-Route::resource('tasks', 'TasksController');
+//ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+
+
+
+
+
 
 // Route::get('tasks/{id}', 'TasksController@show');
 // Route::post('tasks', 'TasksController@store');
