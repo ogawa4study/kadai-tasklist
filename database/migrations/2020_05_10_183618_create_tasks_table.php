d1dcd72b660a6c9cb931fef0b31914369b294763
ogawa4study <ogawa4study@gmail.com>
2020-05-16 12:40:09 +0000

    ReRe add login

--- database/migrations/2020_05_10_183618_create_tasks_table.php
+++ database/migrations/2020_05_10_183618_create_tasks_table.php
@@ -15,12 +15,12 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->index();
+            // $table->integer('user_id')->unsigned()->index();→add_users_id_tableへ
             $table->string('content');
             $table->timestamps();
             
-            //外部キー制約
-            $table->foreign('user_id')->references('id')->on('users');
+            // //外部キー制約
+            // $table->foreign('user_id')->references('id')->on('users');→add_users_id_tableへ
         });
     }
 
